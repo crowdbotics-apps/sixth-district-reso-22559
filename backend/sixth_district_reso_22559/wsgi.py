0a08c1fdbc30fff238e16a185df4ffa5920b952c
Crowdbotics <team@crowdbotics.com>
2020-11-12 20:33:31 +0000

    Update models

--- backend/sixth_district_reso_22559/wsgi.py
+++ backend/sixth_district_reso_22559/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sixth_district_reso_22559.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sixth_district_reso_22559.settings")
 
 application = get_wsgi_application()
